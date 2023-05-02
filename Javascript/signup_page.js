1523d608b658dede4d024989950840118ede5a53
RohanKamat14 <rohannkamat@gmail.com>
2023-05-01 23:44:50 -0600

    EOD

--- Javascript/signup_page.js
+++ Javascript/signup_page.js
@@ -4,4 +4,21 @@ function myFunction() {
     var i;
     console.log(x)
     // document.getElementById("demo").innerHTML = text;
+    var textFile = null,
+    makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    // returns a URL you can use as a href
+    console.log(textFile)
+    return textFile;
+    
+  };
 }
