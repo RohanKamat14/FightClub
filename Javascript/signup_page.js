ede5372a9beaadac870de71c23f2fb12d5e5b8c6
RohanKamat14 <rohannkamat@gmail.com>
2023-05-01 23:10:07 -0600

    test

---
+++ Javascript/signup_page.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.getElementById("name").value;
+    var text = "";
+    var i;
+    console.log(x)
+    // document.getElementById("demo").innerHTML = text;
+}
