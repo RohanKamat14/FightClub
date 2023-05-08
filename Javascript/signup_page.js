38af67b85a6cb7f683db089516037b8bfe923660
RohanKamat14 <rohannkamat@gmail.com>
2023-05-08 16:56:40 -0600

    please

--- Javascript/signup_page.js
+++ Javascript/signup_page.js
@@ -21,42 +21,42 @@ function SendMessage() {
   var data = new FormData();
   data.append('file', input.files[0])
   data.append('user', 'hubot')
-  // if (input.files[0] != undefined) {
-  //   fetch('https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF', {
-  //     method: 'POST',
-  //     body: data,
-  //   })
-  //   fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
-  //   body: JSON.stringify({
-  //     content: final,
-  //   }),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   method: "POST",
-  // })
-  //   .then(function (res) {
-  //     console.log(res);
-  //   })
-  //   .catch(function (res) {
-  //     console.log(res);
-  //   });
-  //   return false
-  // }
-  // fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
-  //   body: JSON.stringify({
-  //     content: final,
-  //   }),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   method: "POST",
-  // })
-  //   .then(function (res) {
-  //     console.log(res);
-  //   })
-  //   .catch(function (res) {
-  //     console.log(res);
-  //   });
+  if (input.files[0] != undefined) {
+    fetch('https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF', {
+      method: 'POST',
+      body: data,
+    })
+    fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
+    body: JSON.stringify({
+      content: final,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+    return false
+  }
+  fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
+    body: JSON.stringify({
+      content: final,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
     return false
 }
