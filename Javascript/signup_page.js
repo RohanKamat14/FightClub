41d8829adc84e343a8968de283d995d64539572b
RohanKamat14 <rohannkamat@gmail.com>
2023-05-08 16:42:49 -0600

    Signup page done

--- Javascript/signup_page.js
+++ Javascript/signup_page.js
@@ -1,24 +1,62 @@
-function myFunction() {
-    var x = document.getElementById("name").value;
-    var text = "";
-    var i;
-    console.log(x)
-    // document.getElementById("demo").innerHTML = text;
-    var textFile = null,
-    makeTextFile = function (text) {
-    var data = new Blob([text], {type: 'text/plain'});
-
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-
-    textFile = window.URL.createObjectURL(data);
-
-    // returns a URL you can use as a href
-    console.log(textFile)
-    return textFile;
-    
-  };
+function SendMessage() {
+  var input = document.querySelector('input[type="file"]');
+  var name = document.getElementById('name').value;
+  var weight = document.getElementById('weight').value;
+  var height = document.getElementById('height').value;
+  var specialty = document.getElementById('specialty').value;
+  var socials = document.getElementById('socials').value;
+  if (name == "") {
+    return;
+  }
+  if (weight == "") {
+    return;
+  }
+  if (height == "") {
+    return;
+  }
+  if (specialty == "") {
+    return;
+  }
+  var final = "Name:" + "\n" + name + "\n" + "Weight:" + "\n" + weight + "\n" + "Height:" + "\n" + height + "\n" + "Specialty:" + "\n" + specialty + "\n" + "Socials:" + "\n" + socials;
+  var data = new FormData();
+  data.append('file', input.files[0])
+  data.append('user', 'hubot')
+  if (input.files[0] != undefined) {
+    fetch('https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF', {
+      method: 'POST',
+      body: data,
+    })
+    fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
+    body: JSON.stringify({
+      content: final,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+    return false
+  }
+  fetch("https://discord.com/api/webhooks/1103904206240362506/jSv_ZQitEtQni_bgRPZ7szaTSzLMs7K2VeAqLtD6P2fFHXA4GNs9h7Qa64Yj0QOP5aSF", {
+    body: JSON.stringify({
+      content: final,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+    return false
 }
